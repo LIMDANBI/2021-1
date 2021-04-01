a15836d8842d580cff759c7276e4ba640ee5606a
dan_bibibi <dlaeksql08222@gmail.com>
2021-04-02 03:45:14 +0900

    sudoku soloution validator

--- operatingSystem/sudoku.c
+++ operatingSystem/sudoku.c
@@ -7,9 +7,12 @@
 #include <time.h>
 #include <pthread.h>
 
-/*
- * 기본 스도쿠 퍼즐
- */
+typedef struct {
+	int row;
+	int column;		
+} parameters;
+
+// 기본 스도쿠 퍼즐
 int sudoku[9][9] = {{6, 3, 9, 8, 4, 1, 2, 7, 5},
                     {7, 2, 4, 9, 5, 3, 1, 6, 8},
                     {1, 8, 5, 7, 2, 6, 3, 9, 4},
@@ -19,13 +22,12 @@ int sudoku[9][9] = {{6, 3, 9, 8, 4, 1, 2, 7, 5},
                     {5, 4, 2, 3, 9, 8, 7, 1, 6},
                     {3, 1, 8, 6, 7, 5, 9, 4, 2},
                     {9, 6, 7, 2, 1, 4, 8, 5, 3}};
-
 /*
  * valid[0][0], valid[0][1], ..., valid[0][8]: 각 행이 올바르면 1, 아니면 0
  * valid[1][0], valid[1][1], ..., valid[1][8]: 각 열이 올바르면 1, 아니면 0
  * valid[2][0], valid[2][1], ..., valid[2][8]: 각 3x3 그리드가 올바르면 1, 아니면 0
  */
-int valid[3][9];
+int valid[3][9] = { 0, };
 
 /*
  * 스도쿠 퍼즐의 각 행이 올바른지 검사한다.
@@ -33,7 +35,25 @@ int valid[3][9];
  */
 void *check_rows(void *arg)
 {
-    // 여기를 완성하세요
+    for(int i=0; i<9; i++){
+        int num[9] = {0, }, isvalid=1;
+        for(int j=0; j<9; j++){
+            if(sudoku[i][j] < 0 || 9 < sudoku[i][j]) {
+                fprintf(stderr, "잘못된 값이 sdoku 배열에 들어있습니다.");
+                pthread_exit(NULL);
+            }
+            else num[sudoku[i][j]-1]++;
+        }
+        for(int j=0; j<9; j++){
+            if(num[j]!=1){
+                isvalid = 0;
+                break;
+            }
+        }
+        if(isvalid) valid[0][i] = 1;
+        else valid[0][i] = 0;
+    }
+    pthread_exit(0);
 }
 
 /*
@@ -42,7 +62,25 @@ void *check_rows(void *arg)
  */
 void *check_columns(void *arg)
 {
-    // 여기를 완성하세요
+    for(int i=0; i<9; i++){
+        int num[9] = {0, }, isvalid=1;
+        for(int j=0; j<9; j++){
+            if( sudoku[j][i] < 0 || 9 < sudoku[j][i]) {
+                fprintf(stderr, "잘못된 값이 sdoku 배열에 들어있습니다.");
+                pthread_exit(NULL);
+            }
+            else num[sudoku[j][i]-1]++;
+        }
+        for(int j=0; j<9; j++){
+            if(num[j]!=1){
+                isvalid = 0;
+                break;
+            }
+        }
+        if(isvalid) valid[1][i] = 1;
+         else valid[1][i] = 0;
+    }
+    pthread_exit(0);
 }
 
 /*
@@ -50,9 +88,33 @@ void *check_columns(void *arg)
  * 3x3 서브그리드 번호는 0부터 시작하며, 왼쪽에서 오른쪽으로, 위에서 아래로 증가한다.
  * k번 서브그리드가 올바르면 valid[2][k]에 1을 기록한다.
  */
-void *check_subgrid(void *arg)
+void *check_subgrid(void *arg) 
 {
-    // 여기를 완성하세요
+    int num[9] = {0, }, isvalid=1;
+
+    parameters *args = (parameters*) arg;
+    int row = args->row;
+    int col = args->column;
+
+    for(int i=row; i<row+3; i++){
+        for(int j=col; j<col+3; j++){
+            if( sudoku[i][j] < 0 || 9 < sudoku[i][j]) {
+                fprintf(stderr, "잘못된 값이 sdoku 배열에 들어있습니다.");
+                pthread_exit(NULL);
+            }
+            else num[sudoku[i][j]-1]++;
+        }
+    }
+    for(int i=0; i<9; i++){
+        if(num[i]!=1){
+            isvalid = 0;
+            break;
+        }
+    }
+    // printf("\n%d", row+col/3);
+    if(isvalid) valid[2][row+col/3] = 1;
+    else valid[2][row+col/3] = 0;
+    pthread_exit(0);
 }
 
 /*
@@ -63,61 +125,61 @@ void *check_subgrid(void *arg)
 void check_sudoku(void)
 {
     int i, j;
+    pthread_t p_thread[11];
 
-    /*
-     * 검증하기 전에 먼저 스도쿠 퍼즐의 값을 출력한다.
-     */
-    for (i = 0; i < 9; ++i)
+    for (i = 0; i < 9; ++i) //검증하기 전에 먼저 스도쿠 퍼즐의 값을 출력한다.
     {
         for (j = 0; j < 9; ++j)
             printf("%2d", sudoku[i][j]);
         printf("\n");
     }
     printf("---\n");
-    /*
-     * 스레드를 생성하여 각 행을 검사하는 check_rows() 함수를 실행한다.
-     */
-    // 여기를 완성하세요
-    /*
-     * 스레드를 생성하여 각 열을 검사하는 check_columns() 함수를 실행한다.
-     */
-    // 여기를 완성하세요
+
+    // 스레드를 생성하여 각 행을 검사하는 check_rows() 함수를 실행
+    pthread_create(&p_thread[0], NULL, check_rows, NULL);
+
+    // 스레드를 생성하여 각 열을 검사하는 check_columns() 함수를 실행
+    pthread_create(&p_thread[1], NULL,check_columns, NULL);
+
     /*
      * 9개의 스레드를 생성하여 각 3x3 서브그리드를 검사하는 check_subgrid() 함수를 실행한다.
      * 3x3 서브그리드의 위치를 식별할 수 있는 값을 함수의 인자로 넘긴다.
      */
-    // 여기를 완성하세요
-    /*
-     * 11개의 스레드가 종료할 때까지 기다린다.
-     */
-    // 여기를 완성하세요
-    /*
-     * 각 행에 대한 검증 결과를 출력한다.
-     */
+    for(int i=0; i<7; i+=3){
+        for(int j=0; j<7; j+=3){
+            int tidx=2;
+            parameters *data = (parameters *)malloc(sizeof(parameters));
+            data->row = i;
+            data->column = j;
+            pthread_create(&p_thread[i+j/3+2], NULL, check_subgrid, data);
+        }
+    }
+
+    // 11개의 스레드가 종료할 때까지 기다린다
+    for(int i=0; i<11; i++){
+        pthread_join(p_thread[i], NULL);
+    }
+
+    // 각 행에 대한 검증 결과를 출력한다.
     printf("ROWS: ");
     for (i = 0; i < 9; ++i)
         printf(valid[0][i] == 1 ? "(%d,YES)" : "(%d,NO)", i);
     printf("\n");
-    /*
-     * 각 열에 대한 검증 결과를 출력한다.
-     */
+
+    // 각 열에 대한 검증 결과를 출력한다.
     printf("COLS: ");
     for (i = 0; i < 9; ++i)
         printf(valid[1][i] == 1 ? "(%d,YES)" : "(%d,NO)", i);
     printf("\n");
-    /*
-     * 각 3x3 서브그리드에 대한 검증 결과를 출력한다.
-     */
+
+    // 각 3x3 서브그리드에 대한 검증 결과를 출력한다.
     printf("GRID: ");
     for (i = 0; i < 9; ++i)
         printf(valid[2][i] == 1 ? "(%d,YES)" : "(%d,NO)", i);
     printf("\n---\n");
 }
 
-/*
- * 스도쿠 퍼즐의 값을 3x3 서브그리드 내에서 무작위로 섞는 함수이다.
- */
-void *shuffle_sudoku(void *arg)
+void *shuffle_sudoku(void *arg) // 스도쿠 퍼즐의 값을 3x3 서브그리드 내에서 무작위로 섞는 함수
 {
     int i, tmp;
     int grid;
@@ -127,33 +189,27 @@ void *shuffle_sudoku(void *arg)
     srand(time(NULL));
     for (i = 0; i < 100; ++i)
     {
-        /*
-         * 0부터 8번 사이의 서브그리드 하나를 무작위로 선택한다.
-         */
+        // 0부터 8번 사이의 서브그리드 하나를 무작위로 선택한다.
         grid = rand() % 9;
-        /*
-         * 해당 서브그리드의 좌측 상단 행열 좌표를 계산한다.
-         */
+
+        // 해당 서브그리드의 좌측 상단 행열 좌표를 계산
         row1 = row2 = (grid / 3) * 3;
         col1 = col2 = (grid % 3) * 3;
-        /*
-         * 해당 서브그리드 내에 있는 임의의 두 위치를 무작위로 선택한다.
-         */
+
+        //해당 서브그리드 내에 있는 임의의 두 위치를 무작위로 선택
         row1 += rand() % 3;
         col1 += rand() % 3;
         row2 += rand() % 3;
         col2 += rand() % 3;
-        /*
-         * 홀수 서브그리드이면 두 위치에 무작위 수로 채우고,
-         */
+
+        // 홀수 서브그리드이면 두 위치에 무작위 수로 채우고,
         if (grid & 1)
         {
             sudoku[row1][col1] = rand() % 8 + 1;
             sudoku[row2][col2] = rand() % 8 + 1;
         }
-        /*
-         * 짝수 서브그리드이면 두 위치에 있는 값을 맞바꾼다.
-         */
+
+        // 짝수 서브그리드이면 두 위치에 있는 값을 맞바꿈
         else
         {
             tmp = sudoku[row1][col1];
@@ -164,28 +220,22 @@ void *shuffle_sudoku(void *arg)
     pthread_exit(NULL);
 }
 
-/*
- * 메인 함수는 위에서 작성한 함수가 올바르게 동작하는지 검사하기 위한 것으로 수정하면 안 된다.
- */
-int main(void)
+
+int main(void) // 위에서 작성한 함수가 올바르게 동작하는지 검사
 {
     int tmp;
     pthread_t tid;
 
-    /*
-     * 기본 스도쿠 퍼즐을 출력하고 검증한다.
-     */
+    // 기본 스도쿠 퍼즐을 출력하고 검증한다.
     check_sudoku();
-    /*
-     * 기본 퍼즐에서 값 두개를 맞바꾸고 검증해본다.
-     */
+
+    // 기본 퍼즐에서 값 두개를 맞바꾸고 검증해본다.
     tmp = sudoku[5][3];
     sudoku[5][3] = sudoku[6][2];
     sudoku[6][2] = tmp;
     check_sudoku();
-    /*
-     * 기본 스도쿠 퍼즐로 다시 바꾼 다음, shuffle_sudoku 스레드를 생성하여 퍼즐을 섞는다.
-     */
+    
+    // 기본 스도쿠 퍼즐로 다시 바꾼 다음, shuffle_sudoku 스레드를 생성하여 퍼즐을 섞는다.
     tmp = sudoku[5][3];
     sudoku[5][3] = sudoku[6][2];
     sudoku[6][2] = tmp;
@@ -194,17 +244,14 @@ int main(void)
         fprintf(stderr, "pthread_create error: shuffle_sudoku\n");
         exit(-1);
     }
-    /*
-     * 무작위로 섞는 중인 스도쿠 퍼즐을 검증해본다.
-     */
+
+    // 무작위로 섞는 중인 스도쿠 퍼즐을 검증해본다.
     check_sudoku();
-    /*
-     * shuffle_sudoku 스레드가 종료될 때까지 기다란다.
-     */
+    
+    // shuffle_sudoku 스레드가 종료될 때까지 기다란다.
     pthread_join(tid, NULL);
-    /*
-     * shuffle_sudoku 스레드 종료 후 다시 한 번 스도쿠 퍼즐을 검증해본다.
-     */
+
+    // shuffle_sudoku 스레드 종료 후 다시 한 번 스도쿠 퍼즐을 검증해본다.
     check_sudoku();
     exit(0);
 }
